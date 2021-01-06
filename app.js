2250e3c2ee555ea801905c4b1483bf85298559d4
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-01-06 18:45:49 +0530

    fix: folder change

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 window.addEventListener("keydown", function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
